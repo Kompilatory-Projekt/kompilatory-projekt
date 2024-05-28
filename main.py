29f8742c316ef8cb42b8b80f9e870977241f8e79
Kacper Rabczewski <krabczewski@gmail.com>
2024-05-28 20:29:06 +0200

    add cpp code formatting

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Python3Lexer import Python3Lexer
 from Python3Parser import Python3Parser
 from Python3ParserVisitor import Python3ParserVisitor
 
-from utils import print_tree
+from utils import print_tree, format_cpp_code
 
 def main(argv):
     input_code = ''''''
@@ -22,7 +22,26 @@ def main(argv):
 
     visitor = Python3ParserVisitor()
     output_code = visitor.visit(tree)
+    
+    output_code = format_cpp_code(output_code)
+    
     print(f"\nOUTPUT:\n{output_code}")
+    
+
+def convert_code(input_code: str):
+    input_stream = InputStream(input_code)
+    lexer = Python3Lexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = Python3Parser(stream)
+    
+    tree = parser.file_input()
+
+    visitor = Python3ParserVisitor()
+    output_code = visitor.visit(tree)
+    
+    output_code = format_cpp_code(output_code)
+    
+    return output_code
 
 if __name__ == "__main__":
     main(sys.argv)
